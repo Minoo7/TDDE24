8431e06f57592454b5be8dd72d5e0a636305d7fb
minoo7 <accounts@minoo.pro>
2021-12-27 04:20:14 +0100

    klar med 2012 tenta

--- tenta/2012_12_18/code.py
+++ tenta/2012_12_18/code.py
@@ -66,10 +66,122 @@ def average_max(seq: list) -> int:
  Uppgift 4
 -----------------------------------------------------------------------------"""
 
-def makering(seq: list) -> "ring":
+def make_ring(seq: list) -> "ring":
     """make_ring : list of elements -> ring"""
-    return 
-    pass
+    """Skapar och returnerar en ny ring från en lista med element där första
+    elementet i listan är toppelement i ringen."""
+    return seq
+
+def is_ring(obj) -> bool:
+    """Kontrollerar om det givna objektet är en ring."""
+    return isinstance(obj, list)
+
+def top(obj: "ring") -> "top_ring":
+    """Returnerar toppelementet i ringen."""
+    if is_ring(obj):
+        return obj[0]
+
+def left_rotate(ring) -> "ring":
+    """Returnerar en ny ring genom att elementen roteras ett steg åt vänster."""
+    if is_ring(ring):
+        seq = ring[1:] + [ring[0]]
+        return make_ring(seq)
+
+def right_rotate(ring) -> "ring":
+    """Returnerar en ny ring genom att elementen roteras ett steg åt höger."""
+    if is_ring(ring):
+        seq = [ring[-1]] + ring[:-1]
+        return make_ring(seq)
+
+def left_rotate_in(ring):
+    """Roterar elementen i den aktuella ringen ett steg åt vänster. Inga
+    nya objekt ska skapas eller returneras, utan ringen ska modifieras
+    på plats."""
+    if is_ring(ring):
+        last = ring.pop(0)
+        ring.append(last)
+
+def right_rotate_in(ring):
+    """Roterar elementen i den aktuella ringen ett steg åt höger. Inga
+    nya objekt ska skapas eller returneras, utan ringen ska modifieras
+    på plats."""
+    if is_ring(ring):
+        first = ring.pop(len(ring) - 1)
+        ring.insert(0, first)
+"""-----------------------------------------------------------------------------
+ Uppgift 5
+-----------------------------------------------------------------------------"""
+animals = {
+    1: ("Var lever djuret?", ("på land", 2), ("i vatten", 3), ("i luften", 4)),
+    2: ("Hur många ben har djuret?", ("två", 5), ("fyra", 6), ("många", 7)),
+    3: "en fisk",
+    4: ("Ungefär hur stort är det?", ("litet", 8), ("stort", 9)),
+    5: "en apa",
+    6: ("Hur ser djuret ut utanpå?", ("taggar", 10), ("päls", 11)),
+    7: "en insekt av något slag",
+    8: ("Hur ser djuret ut?", ("grått", 12), ("rött bröst", 13), ("blå och gul", 14)),
+    9: ("Hur beter sig djuret?", ("glidflyger", 15), ("hoar om natten", 16)),
+    10: "en igelkott",
+    11: ("Hur låter djuret?", ("miau", 17), ("vov", 18)),
+    12: "en gråsparv",
+    13: "en domherre",
+    14: "en blåmes",
+    15: "en örn",
+    16: "en uggla",
+    17: "en katt",
+    18: "en hund"}
+
+def expert(data):
+    key = 1
+    while True:
+        values = data[key]
+        if isinstance(values, tuple):
+            for line_vals in enumerate(values):
+                line = line_vals[1]
+                if isinstance(line, tuple):
+                    print(f"  {line_vals[0]}. {line[0]}")
+                else:
+                    print(line)
+        else:
+            print(f"Jag tror att det är {values}.")
+            break
+        choice = int(input())
+        key = values[choice][1]
+
+"""-----------------------------------------------------------------------------
+ Uppgift 6
+-----------------------------------------------------------------------------"""
+
+def count_i(seq):
+    result = []
+    amount = 1
+    prev = seq[0]
+    for vals in enumerate(seq[1:]):
+        val = vals[1]
+        if val == prev:
+            amount += 1
+            if vals[0] == len(seq) - 2: #if last
+                result.append((val, amount))
+        else:
+            result.append((prev, amount))
+            amount = 1
+            prev = val
+    return result
+
+def count_r(seq):
+    def inner(seq, prev, amount):
+        if not seq:
+            if amount > 0:
+                return [(prev, amount)]
+            return []
+            #if amount > 0
+        if seq[0] == prev:
+            return inner(seq[1:], prev, amount+1)
+        else:
+            return [((prev), amount)] + inner(seq[1:], seq[0], 1)
+        
+    return inner(seq[1:], seq[0], 1)
+        
 
 if __name__ == '__main__':
     assert echo("Korvbröd") == 'Korvbröd bröd öd'
