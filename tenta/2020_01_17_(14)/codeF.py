d087073264c960946cd8c2934649910115c02056
minoo7 <accounts@minoo.pro>
2022-01-07 13:04:28 +0100

    merge uppgift 2019

--- tenta/2020_01_17_(14)/codeF.py
+++ tenta/2020_01_17_(14)/codeF.py
@@ -16,5 +16,31 @@ def cartprod(sets): #funkar inte för alla
         return [tuple([first[0]] + [sub_set] + cartprod(rest[1:])) for sub_set in rest[0]]
     return first
 
+#Uppgift 1
+def divide_i1(n, seq):
+    """Delar upp element i en lista i totalt n givna listor"""
+    lst = []
+    #amount = len(seq) / n
+    amount = 2
+    count = 0
+    sub_lst = []
+    for ele in enumerate(seq):
+        sub_lst.append(ele[1])
+        indx = ele[0]+1
+        if indx - count == amount:
+            lst.append(sub_lst)
+            sub_lst = []
+            count = indx
+    return lst
+
+def divide_i(n, seq):
+    lst = []
+    for i in range(len(n)):
+        #for ele in range()
+        pass
+    pass
+
 if __name__ == '__main__':
-    print(cartprod([{1,2}, {3,4,5}, {11,8}]))
+    #print(cartprod([{1,2}, {3,4,5}, {11,8}]))
+    print(divide_i(3, [1, 2, 3, 4, 5]))
+
